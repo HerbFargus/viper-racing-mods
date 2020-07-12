653776c5a7c98a15300612dd9dac8fbfd05e2e68
HerbFargus <fargusherb@gmail.com>
2020-07-12 16:47:25 -0700

    initial commits
    add decompiler files for tracks

---
+++ rescrack.bat
@@ -0,0 +1,21 @@
+@echo off
+color 0a
+rescrack nfield.trk
+echo.
+echo.
+echo.
+echo EXTRACTION COMPLETE
+echo.
+echo.
+echo.
+echo.
+CALL  tex2tga.bat
+echo.
+echo.
+echo.
+echo REQUESTED OPERATIONS COMPLETE
+echo.
+echo.
+echo.
+pause
+exit/b
---
+++ tex2tga.bat
@@ -0,0 +1,7 @@
+@echo off
+convert *.tex *.tga
+echo                          ALSO ALL THE
+echo           .tex FILES CONVERTED SUCCESSFULLY
+echo                      TO .tga IMAGE FILES
+pause
+exit/b
